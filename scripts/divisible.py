562a41d1b89e99cdc74f2da317729980c48cc014
hsajadi <afrang.sajadian@gmail.com>
2023-02-10 14:27:46 +0100

    added sulotion to excersisess

--- scripts/divisible.py
+++ scripts/divisible.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python
-
 print("Please enter a number and press ENTER")
 
 # Define the number eleven​
 number = int(input())
 # As long as result is larger than zero:​
 while number > 0:
-
-   # Subtract three​
-   number = number - 3
+    # Subtract three​
+    number = number - 3
 
 # Check if is zero or not​
 if number == 0:
 
-  # print result
-     print ("Number is divisible by 3")
+    # print result
+    print("Number is divisible by 3")
 
 else:
 
-    print("Number is NOT divisible by 3")  
-
+    print("Number is NOT divisible by 3")
