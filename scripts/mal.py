bdb37c0becb57268dd695a22b6295a518a65e9b8
hsajadi <afrang.sajadian@gmail.com>
2023-02-14 09:20:01 +0100

    added mai

---
+++ scripts/mal.py
@@ -0,0 +1,12 @@
+def mal(param1, param2):
+    """
+    deze functie berekent de mal van twee getallen:
+    param1 * param2
+    """
+    print(param1, "mal", param2, " = ", param1 * param2)
+
+    return param1 * param2
+
+
+print(mal.__doc__)
+mal(15, 12)
