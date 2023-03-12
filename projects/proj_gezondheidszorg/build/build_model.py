418d6e66660f0043843c8a125f4068e71bb25e9c
AlxcNL <J.A.Boogaard@hr.nl>
2023-03-12 22:00:40 +0100

    Improved example of build and data pipeline

--- projects/proj_gezondheidszorg/build/build_model.py
+++ projects/proj_gezondheidszorg/build/build_model.py
@@ -1,36 +1,49 @@
 #!/usr/bin/env python
 
+# Imports
 import logging
-import numpy as np
 import pandas as pd
 import pickle
-import statsmodels.api as sm
 import sqlite3
+
+# Hardening
+from pathlib import Path
+from sklearn.neighbors import KNeighborsRegressor
 
 # Global configuration
 logging.basicConfig(level=logging.DEBUG)
+exportFile = "../models/knn.pkl"
 
 dbName = "../rest_server/medisch_centrum_randstad/db.sqlite3"
 tableName = "rest_api_netlify"
 
-logging.info("Load transformed data from database into dataframe")
+logging.info(f"Connect to {Path(dbName).name}")
 dbConnection = sqlite3.connect(dbName)
-dfFromDB = pd.read_sql_query(f"SELECT * FROM {tableName}", dbConnection)
+
+logging.info("Load transformed data from database into dataframe")
+dfFromDB = pd.read_sql_query(f"SELECT length, lifespan FROM {tableName}", dbConnection)
+
+logging.info("Close DB Connection")
+dbConnection.close()
+
+logging.info("Preprocessing : remove rows with missing values")
+dfCleanFromDB = dfFromDB.dropna()
+logging.debug(dfCleanFromDB.head())
+
+logging.info("Feature Selection")
+
+y = dfCleanFromDB['lifespan'].values
+logging.debug(f"Y : {type(y)}")
+
+X = dfCleanFromDB['length'].values
+logging.debug(f"X : {type(X)}")
 
 logging.info("Build Regression Model")
 
-Y = dfFromDB['lifespan']
-logging.debug(f"Y : {Y}")
+regressor = KNeighborsRegressor(n_neighbors=4, metric='euclidean')
+regressor.fit(X.reshape(-1, 1), y)
 
-X = dfFromDB['length']
-logging.debug(f"X : {X}")
+logging.debug(f"Check with single value {regressor.predict([[185]])}")
 
-# model = sm.OLS(y, X).fit()
-duncan_prestige = sm.datasets.get_rdataset("Duncan", "carData")
-Y = duncan_prestige.data['income']
-X = duncan_prestige.data['education']
-X = sm.add_constant(X)
-model = sm.OLS(Y, X)
-
-# pickle.dump(self.model, open(exportFile,'wb'))
-    
+pickle.dump(regressor, open(exportFile,'wb'))
+dbConnection.close()
