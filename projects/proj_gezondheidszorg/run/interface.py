60c57e1ab73c1186ff30b1481355e273fb817d4e
AlxcNL <J.A.Boogaard@hr.nl>
2023-03-15 14:42:27 +0100

    Added predictions

--- projects/proj_gezondheidszorg/run/interface.py
+++ projects/proj_gezondheidszorg/run/interface.py
@@ -9,15 +9,30 @@ import sqlite3
 
 # Global configuration
 logging.basicConfig(level=logging.DEBUG)
+regModel = '../models/model_pkl'
+
+logging.info("Load model")
+with open(regModel, 'rb') as f:
+    regressor = pickle.load(f)
+
+
+def predict(inputData):
+
+    # Predict lifespan with regressor and inputData
+    prediction = regressor(inputData)
+    return prediction
+
 
 def inputDigit(message, acceptableRange):
     inputStr = str()
     withinRange = False
     numberOfTries = 3
+    inputNum = None
     i = 0
 
     while not (inputStr.isdigit() and withinRange) and i < numberOfTries:
         inputStr = input(message)
+        logging.debug(inputStr)
 
         if inputStr.isdigit():
             inputNum = float(inputStr)
@@ -26,6 +41,9 @@ def inputDigit(message, acceptableRange):
                 return inputNum
 
         i += 1
+
+    return None
+
 
 acceptableRange = range(18, 118)
 age = int(inputDigit("Age [18 - 118]: ", acceptableRange))
