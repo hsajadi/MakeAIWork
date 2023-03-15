fac171a7e3a7a5c6c4df95a1cf746ad1c3c29f5e
AlxcNL <J.A.Boogaard@hr.nl>
2023-03-15 07:12:07 +0100

    Update interface

--- projects/proj_gezondheidszorg/run/interface.py
+++ projects/proj_gezondheidszorg/run/interface.py
@@ -10,8 +10,23 @@ import sqlite3
 # Global configuration
 logging.basicConfig(level=logging.DEBUG)
 
-lengthStr = input("Enter your length : ")
+def inputDigit(message, acceptableRange):
+    inputStr = str()
+    withinRange = False
+    numberOfTries = 3
+    i = 0
 
-if lengthStr.isdecimal():
-    length = float(lengthStr)
-    logging.debug(length)
+    while not (inputStr.isdigit() and withinRange) and i < numberOfTries:
+        inputStr = input(message)
+
+        if inputStr.isdigit():
+            inputNum = float(inputStr)
+
+            if inputNum in acceptableRange:
+                return inputNum
+
+        i += 1
+
+acceptableRange = range(18, 118)
+age = int(inputDigit("Age [18 - 118]: ", acceptableRange))
+logging.debug(f"age : {age}")
