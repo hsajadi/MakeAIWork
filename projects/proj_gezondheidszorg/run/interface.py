21cba3b16fe27483763e0428191e936d5e317bf3
AlxcNL <J.A.Boogaard@hr.nl>
2023-03-17 06:58:18 +0100

    Finished interface

--- projects/proj_gezondheidszorg/run/interface.py
+++ projects/proj_gezondheidszorg/run/interface.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python
 
+from pathlib import Path
 import logging
+import numpy as np
 import pandas as pd
 import pickle
 import statsmodels as sm
 import statsmodels.formula.api as smf
-import sqlite3
 
 # Global configuration
 logging.basicConfig(level=logging.DEBUG)
-regModel = '../models/model_pkl'
-
-logging.info("Load model")
-with open(regModel, 'rb') as f:
-    regressor = pickle.load(f)
-
-
-def predict(inputData):
-
-    # Predict lifespan with regressor and inputData
-    prediction = regressor(inputData)
-    return prediction
-
+regModel = '../models/knn.pkl'
 
 def inputDigit(message, acceptableRange):
     inputStr = str()
@@ -33,18 +22,54 @@ def inputDigit(message, acceptableRange):
     while not (inputStr.isdigit() and withinRange) and i < numberOfTries:
         inputStr = input(message)
         logging.debug(inputStr)
-
+        
         if inputStr.isdigit():
             inputNum = float(inputStr)
 
             if inputNum in acceptableRange:
                 return inputNum
+            else:
+                print("Enter a value between %s and %s" %(min(acceptableRange), max(acceptableRange)))
 
         i += 1
 
     return None
 
+def inputPatientData():
+    logging.debug("Start dialog")
+    
+    length = int(inputDigit("Length : ", range(90, 220)))
+    logging.debug(f"length : {length}")
 
-acceptableRange = range(18, 118)
-age = int(inputDigit("Age [18 - 118]: ", acceptableRange))
-logging.debug(f"age : {age}")
+    weigth = int(inputDigit("Weigth : ", range(40, 140)))
+    logging.debug(f"weigth : {weigth}")
+    
+    # Return tuple
+    return (length, weigth)
+
+def predictLifespan(bmi):
+    # Default return value
+    prediction = None
+    
+    logging.info(f"Load regression model {Path(regModel).name}")
+    with open(regModel, 'rb') as f:
+        regressor = pickle.load(f)
+
+    logging.debug("Predict lifespan with Regressor and BMI")
+    X = np.array(bmi)
+    prediction = int(regressor.predict(X.reshape(-1, 1))[0])
+
+    return prediction     
+
+(length, weigth) = inputPatientData()
+
+logging.info("Determine BMI")
+# BMI = (Weight in Kilograms / (Height in Meters x Height in Meters))
+bmi = weigth / pow(length/100, 2) if (length > 0) else None    
+
+if not bmi is None:
+    predLifespan = predictLifespan(bmi)
+
+if not predictLifespan is None:
+    print(f"Estimated lifespan : {predLifespan}")    
+    
